3e8941adb1b7b1969f4e0366c503b919ed945786
YossiSaadi <30905362+YossiSaadi@users.noreply.github.com>
2019-05-31 20:06:05 +0300

    Added the configApiKeys.js, but an empty one

---
+++ configApiKeys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    TWITTER_API_KEYS: {
+        consumer_key: 'ADD YOURS HERE',  
+        consumer_secret: 'ADD YOURS HERE',        
+        access_token: 'ADD YOURS HERE',  
+        access_token_secret: 'ADD YOURS HERE'
+    },
+    GIPHY_API_KEYS: 'ADD YOURS HERE',
+    CMC_API_KEYS: 'ADD YOURS HERE'
+};
